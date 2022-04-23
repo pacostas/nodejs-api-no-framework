ade8a7b661b015cf23f44944c76aceb99c4b07ec
Costas Papastathis <papastathiscr@gmail.com>
2022-04-24 02:05:03 +0300

    updating a post

--- server.js
+++ server.js
@@ -27,6 +27,18 @@ const server = createServer((req, res) => {
         },
       }),
     );
+  } else if (req.url.match(/\/api\/post\/\w+/) && req.method === 'PUT') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        data: {
+          title: 'This is the title',
+          content: 'This is the content',
+          createdBy: 'Bob',
+          dateCreated: '2022-01-01T14:48:00.000Z',
+        },
+      }),
+    );
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Not found' }));
