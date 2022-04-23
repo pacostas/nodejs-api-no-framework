804321f232bbbe25edc6f545a15a58b09b9db24e
Costas Papastathis <papastathiscr@gmail.com>
2022-04-24 02:05:32 +0300

    deleting a post

--- server.js
+++ server.js
@@ -64,6 +64,24 @@ const server = createServer((req, res) => {
         },
       }),
     );
+  } else if (req.url.match(/\/api\/todo\/\w+/) && req.method === 'DELETE') {
+    const id = req.url
+      .split('/')
+      .filter(pathParams => pathParams !== '/')
+      .pop();
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        data: {
+          id: id,
+          title: 'This is the title',
+          content: 'This is the content',
+          createdBy: 'Bob',
+          dateCreated: '2022-01-01T14:48:00.000Z',
+        },
+      }),
+    );
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Not found' }));
