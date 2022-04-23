4fde320db9a7f2a24870a6c9e1c87dff3d793c47
Costas Papastathis <papastathiscr@gmail.com>
2022-04-24 00:53:27 +0300

    serving 404 not found route

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const server = createServer((req, res) => {
         },
       }),
     );
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Not found' }));
   }
 });
 
