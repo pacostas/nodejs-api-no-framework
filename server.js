33a3010b2645967b5065044cb6fc1fa6e7cb96f7
Costas Papastathis <papastathiscr@gmail.com>
2022-05-02 18:24:36 +0300

    connecting to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 import { createServer } from 'http';
+import { MongoClient } from 'mongodb';
 
 const PORT = process.env.PORT || 3000;
+const mongoUser = process.env.MONGO_USER || 'root';
+const mongoPassword = process.env.MONGO_PASSWORD || 'password';
+const mongoHost = process.env.MONGO_URL || '127.0.0.1';
+const mongoPort = process.env.MONGO_PORT || 27017;
+const mongoDBName = process.env.MONGO_DB_NAME || 'myAwesomeBlog';
+
+const mongoDBUri = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`;
+
+const mongoClient = new MongoClient(mongoDBUri);
+
+async function run() {
+  try {
+    // Establish and verify connection
+    await mongoClient.connect();
+    await mongoClient.db(mongoDBName).command({ ping: 1 });
+    console.log('Connected successfully to server');
+  } catch (err) {
+    console.log(err);
+    console.log('Trying to reconnect...');
+    await run();
+  }
+}
+
+run();
 
 const server = createServer((req, res) => {
   if (req.url.match(/\/api\/post\/\w+/) && req.method === 'GET') {
