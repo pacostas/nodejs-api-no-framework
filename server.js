7b5200a95f55b106e9db6c4c962c27c0d5bce5ab
Costas Papastathis <papastathiscr@gmail.com>
2022-05-02 18:24:36 +0300

    connecting to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 import { createServer } from 'http';
+import { MongoClient } from 'mongodb';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+const mongoUser = process.env.MONGO_USER || 'root';
+const mongoPassword = process.env.MONGO_PASSWORD || 'password';
+const mongoHost = process.env.MONGO_URL || '127.0.0.1';
+const mongoPort = process.env.MONGO_PORT || 27017;
+const mongoDBName = process.env.MONGO_DB_NAME || 'todoList';
+
+const mongoDBUri = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`;
+
+const mongoClient = new MongoClient(mongoDBUri);
+
+let db;
+
+async function run() {
+  try {
+    await mongoClient.connect();
+    db = mongoClient.db(mongoDBName);
+    console.log('Connected successfully to database');
+  } catch (err) {
+    console.log('Oh, there is an error in connecting to database');
+    console.log(err);
+    const retrySeconds = 3000;
+    console.log(`Retrying in reconnecting in ${retrySeconds} seconds.`);
+    setTimeout(function () {
+      run();
+    }, retrySeconds * 1000);
+  }
+}
+
+run();
 
 const server = createServer((req, res) => {
   if (req.url.match(/\/api\/todo\/\w+/) && req.method === 'GET') {
