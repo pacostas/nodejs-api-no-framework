add64af8149b8ae195ca09f26e8c6e3e286c0124
Costas Papastathis <papastathiscr@gmail.com>
2022-07-02 14:55:11 +0300

    fixup! creating a new post

--- server.js
+++ server.js
@@ -67,28 +67,15 @@ const server = createServer((req, res) => {
       }),
     );
   } else if (req.url.match(/\/api\/todo/) && req.method === 'POST') {
-
     let data = '';
     req.on('data', chunk => {
       data += chunk;
     });
     req.on('end', () => {
       console.log(JSON.parse(data));
-      res.end();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(data));
     });
-
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-
-    res.end(
-      JSON.stringify({
-        data: {
-          title: 'This is the title',
-          content: 'This is the content',
-          createdBy: 'Bob',
-          dateCreated: '2022-01-01T14:48:00.000Z',
-        },
-      }),
-    );
   } else if (req.url.match(/\/api\/todo\/\w+/) && req.method === 'PUT') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(
