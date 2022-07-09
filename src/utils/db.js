ce69676cd0747db52113d955dd5fa2eb197ae5f3
Costas Papastathis <papastathiscr@gmail.com>
2022-07-10 01:50:05 +0300

    moving db to utils section

---
+++ src/utils/db.js
@@ -0,0 +1,32 @@
+import { MongoClient } from 'mongodb';
+
+const mongoUser = process.env.MONGO_USER || 'root';
+const mongoPassword = process.env.MONGO_PASSWORD || 'password';
+const mongoHost = process.env.MONGO_URL || '127.0.0.1';
+const mongoPort = process.env.MONGO_PORT || 27017;
+const mongoDBName = process.env.MONGO_DB_NAME || 'todoList';
+
+const mongoDBUri = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`;
+const mongoClient = new MongoClient(mongoDBUri);
+
+export let db;
+export let todoCollection;
+
+async function run() {
+  try {
+    await mongoClient.connect();
+    db = mongoClient.db(mongoDBName);
+    todoCollection = db.collection('todo');
+    console.log('Connected successfully to database');
+  } catch (err) {
+    const retrySeconds = 3000;
+    console.log('Oh, there is an error in connecting to database');
+    console.log(err);
+    console.log(`Retrying in reconnecting in ${retrySeconds} seconds.`);
+    setTimeout(function () {
+      run();
+    }, retrySeconds * 1000);
+  }
+}
+
+run();
